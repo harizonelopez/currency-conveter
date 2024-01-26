dd33d5c2ff0dd7e54e66151757eed8ea938400e0
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-01-26 15:29:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # currency conveter program; a project which automatically sends messages to a number provided by the url
 
 import pywhatkit
-
-try:
-    pywhatkit.sendwhatmsg("+254769405130", "Good morning sir, it's JARVIS", 12,10)
-    print("Helloh! Your Message has been sent succesfully.")
-    
-except:
-    print("An error encountered while sending the message")
+def currency_conveter():
+    try:
+        pywhatkit.sendwhatmsg("+254769405130", "Good morning sir, it's JARVIS", 12,10)
+        print("Helloh! Your Message has been sent succesfully.")
+        
+    except:
+        print("An error encountered while sending the message")
+currency_conveter()
