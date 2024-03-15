939e34a1816a14e30a6b6d12649194ef012e5336
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-03-15 15:56:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 # Dev_@ladinh production
-# currency conveter program; this project mainly entails automatically sending messages to a number provided in the url after a period of time you plan it to send the message
+# a currency conveter app
 
-import pywhatkit
+import requests
 
-class Currency:
-    def currency_conveter():
-        try:
-            sender_name = input("Enter your name\n:-> ")
-            receiver_name = input("Enter the receiver's name\n:-> ")            
-            pywhatkit.sendwhatmsg("+254769405130", "Good morning sir, it's JARVIS", 12,10)
-            print(f"Helloh! {sender_name}, Your Message has been sent succesfully to {receiver_name}.")
-            
-        except:
-            print(f"{sender_name}, An error encountered while sending the message to {receiver_name}")
-    currency_conveter()
+def get_exchange_rate(api_key, base_currency, target_currency):
+    try:
+        url = f'https://open.er-api.com/v6/latest/{base_currency}?apikey = {api_key}'
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data['rates'][target_currency]
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the exchange rate: {e}")
+        
+        return None
+
+def convert_currency(api_key, amount, base_currency, target_currency):
+    exchange_rate = get_exchange_rate(api_key, base_currency, target_currency)
+    converted_amount = amount * exchange_rate
+    return converted_amount
+
+def main():
+    api_key = '' #API KEY pending to be inserted here...?
+
+    amount = float(input("\nEnter the amount: "))
+    base_currency = int(input("Enter the source currency code: "))
+    target_currency = int(input("Enter the target currency code: "))
+    
+    exchange_rate = get_exchange_rate(api_key, base_currency, target_currency)
+    
+    if exchange_rate is not None:
+        result = convert_currency(amount, exchange_rate)
+    print(f"{amount} {base_currency} is equal to {result:.2f} {target_currency}\n")
+    
+if __name__=="__main__":
+    main()
